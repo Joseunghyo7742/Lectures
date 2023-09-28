0dc86463bd4b988f4f227f59b8d769b1ecdd9d3e
Jo Seung Hyo <seunghyo7742@naver.com>
2023-09-29 01:38:39 +0900

    실습4: 좌표 007 엘리

--- DreamCoding-Browser/Chapter3/좌표찾아/main.js
+++ DreamCoding-Browser/Chapter3/좌표찾아/main.js
@@ -3,13 +3,13 @@ const img = document.querySelector('img');
 const xLine = document.querySelector('.line-h');
 const yLine = document.querySelector('.line-v');
 
-window.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', (e) => {
   const x = e.clientX;
   const y = e.clientY;
   coordinate.innerHTML = `(${x}px, ${y},px)`;
-  img.style.top = `${y - img.clientHeight / 2-2}px`;
-  img.style.left = `${x - img.clientWidth / 2-8}px`;
-  coordinate.style.top = `${y - 60}px`;
+  img.style.top = `${y}px`;
+  img.style.left = `${x}px`;
+  coordinate.style.top = `${y}px`;
   coordinate.style.left = `${x}px`;
   xLine.style.top = `${y}px`;
   yLine.style.left = `${x}px`;
