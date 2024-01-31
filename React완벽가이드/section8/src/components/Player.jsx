dd820aa41fed756ac2f7fa2c815f535d924cf066
Jo Seung Hyo <seunghyo7742@naver.com>
2024-01-31 16:31:23 +0900

    section8-1

---
+++ React완벽가이드/section8/src/components/Player.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [name,setName]= useState('')
+  const [submitted, setSubmitted] = useState(false)
+  function handleChange(event){
+    setName(event.target.value)
+  }
+  function handleClick(){
+    setSubmitted(true)
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {submitted? name: 'unknown entity'}</h2>
+      <p>
+        <input value={name} type="text" onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
