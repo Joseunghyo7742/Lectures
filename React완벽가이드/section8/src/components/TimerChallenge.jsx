d285b74af49c4157a10495920ab02b6ebd39f2e6
Jo Seung Hyo <seunghyo7742@naver.com>
2024-02-20 15:06:24 +0900

    성공 메시지 모달

--- React완벽가이드/section8/src/components/TimerChallenge.jsx
+++ React완벽가이드/section8/src/components/TimerChallenge.jsx
@@ -16,6 +16,9 @@ function TimerChallenge({title, targetTime}) {
     dialog.current.open()
   }
 
+  function handleReset(){
+    setTimeRemaining(targetTime*1000)
+  }
   function handleStart(){
     timer.current= setInterval(()=>{
       setTimeRemaining(prev=> prev-10)
@@ -29,7 +32,7 @@ function TimerChallenge({title, targetTime}) {
   }
   return (
     <>
-    <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+    <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset}/>
     <section className="challenge">
       <h2>{title}</h2>
       <p className="challenge-time">
