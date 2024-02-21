4b79ec21fb634017436605edbed1814425f8a122
Jo Seung Hyo <seunghyo7742@naver.com>
2024-02-21 19:25:13 +0900

    상황에 따라 띄우는 컴포넌트 달리하기

--- React완벽가이드/section9/src/App.jsx
+++ React완벽가이드/section9/src/App.jsx
@@ -4,10 +4,28 @@ import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="flex h-screen gap-8 my-8">
-      <Sidebar />
-      <NoProjectSelected/>
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- React완벽가이드/section9/src/components/Sidebar.jsx
+++ React완벽가이드/section9/src/components/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Button from './Button';
 
-const Sidebar = () => {
+const Sidebar = ({ onStartAddProject }) => {
   return (
     <aside className="w-1/3 px-8 py-16 rounded-r-xl bg-stone-900 text-stone-50 md:w-72">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200 ">
         Your Projects
       </h2>
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
