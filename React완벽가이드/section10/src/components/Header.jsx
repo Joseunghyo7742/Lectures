2fe2844ea42c8c7406e373e0be40127dabec79fc
Jo Seung Hyo <seunghyo7742@naver.com>
2024-02-24 14:04:33 +0900

    Provider 컴포넌트 생성

--- React완벽가이드/section10/src/components/Header.jsx
+++ React완벽가이드/section10/src/components/Header.jsx
@@ -3,11 +3,11 @@ import { CartContext } from '../store/shopping-cart-context.jsx';
 
 import CartModal from './CartModal.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
   const { items } = useContext(CartContext);
   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -33,7 +33,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
           <h1>Elegant Context</h1>
         </div>
         <p>
-          <button onClick={handleOpenCartClick}>Cart ({items.length})</button>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
         </p>
       </header>
     </>
