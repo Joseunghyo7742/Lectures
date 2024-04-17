4688add35515abcb60f4a80584715cf4020a2da7
Jo Seung Hyo <seunghyo7742@naver.com>
2024-04-17 23:42:55 +0900

    Post

--- React완벽가이드/section15/src/http.js
+++ React완벽가이드/section15/src/http.js
@@ -9,4 +9,19 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
-//utility function 
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }), //자바스크립트 배열은 첨부할 수 없기에  JSON형식으로 바꾸어야함
+    header: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error('failed to update user data.');
+  }
+  return resData.message;
+}
+//utility function
